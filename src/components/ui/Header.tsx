646410c38779aa56011255ac86c4ced7a1741adf
Tracer923 <r.arisaka13@gmail.com>
2025-07-11 14:43:23 +0900

    初回コミット

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
     return (
         <header className="w-full bg-zinc-900 border-b border-zinc-700 shadow-md shadow-zinc-800/30 backdrop-blur-md  relative z-[100]">
             <div className="max-w-5xl mx-auto px-4 py-2 flex justify-between items-center">
-                <h1 className="text-xl font-bold text-white">📚 BookNest</h1>
+                <h1 className="text-xl font-bold text-white">📚 Ink Trace</h1>
                 <NavigationMenu>
                 <NavigationMenuList>
                     <NavigationMenuItem>
@@ -65,4 +65,3 @@ export default function Header() {
         </header>
     )
 }
-
